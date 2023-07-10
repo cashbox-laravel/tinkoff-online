677d4e189eb82d0b0e9a2a9341a68ebb7f93fe12
Andrey Helldar <helldar@dragon-code.pro>
2023-07-10 13:21:11 +0300

    Fixed code-style

--- src/Helpers/Statuses.php
+++ src/Helpers/Statuses.php
@@ -25,24 +25,20 @@ class Statuses extends BaseStatus
         'FORM_SHOWED',
         'NEW',
     ];
-
     public const REFUNDING = [
         'REFUNDING',
     ];
-
     public const REFUNDED = [
         'PARTIAL_REFUNDED',
         'REFUNDED',
         'REVERSED',
     ];
-
     public const FAILED = [
         'ATTEMPTS_EXPIRED',
         'CANCELED',
         'DEADLINE_EXPIRED',
         'REJECTED',
     ];
-
     public const SUCCESS = [
         'CONFIRMED',
     ];
